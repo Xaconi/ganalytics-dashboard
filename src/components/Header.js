43b774907aa81010477a095abfe0d4da3bb89c7b
Xaconi <xaco.89@gmail.com>
2019-03-09 10:38:21 +0100

    [UPD] Changed the social network links column system to Bulma elements.

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,15 +5,15 @@ export class Header extends Component {
         return(
             <header>
                 {/* Logo + Social Network Links */}
-                <div className="icons-container">
-                    <div className="icon-container">
+                <div className="columns">
+                    <div className="column is-offset-4 is-2">
                         <span className="icon is-medium">
                             <a href="https://twitter.com/xaconi" target="_blank" rel="noopener noreferrer">
                                 <i className="fab fa-twitter-square fa-2x"></i>
                             </a>
                         </span>
                     </div>
-                    <div className="icon-container">
+                    <div className="column is-2">
                         <span className="icon is-medium">
                             <a href="https://github.com/Xaconi/ganalytics-dashboard" target="_blank" rel="noopener noreferrer">
                                 <i className="fab fa-github-square fa-2x"></i>
