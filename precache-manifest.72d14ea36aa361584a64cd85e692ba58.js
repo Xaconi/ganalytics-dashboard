5536efaa4afa88b996221288af2e44a93dbd3450
Xaconi <xaco.89@gmail.com>
2019-03-11 23:57:52 +0100

    Updates

---
+++ precache-manifest.72d14ea36aa361584a64cd85e692ba58.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "c39278f7abfc798a241551194f55e29f",
+    "url": "/ganalytics-dashboard/static/media/fa-brands-400.c39278f7.ttf"
+  },
+  {
+    "revision": "adcefe34f10333ec2096",
+    "url": "/ganalytics-dashboard/static/css/main.40c45279.chunk.css"
+  },
+  {
+    "revision": "d93378d1bbda568d28df",
+    "url": "/ganalytics-dashboard/static/js/runtime~main.d93378d1.js"
+  },
+  {
+    "revision": "82905d8d1c06969df11c8c378e9bdd4c",
+    "url": "/ganalytics-dashboard/static/media/fa-solid-900.82905d8d.svg"
+  },
+  {
+    "revision": "a7942393c52e76e3f85e",
+    "url": "/ganalytics-dashboard/static/js/2.a7942393.chunk.js"
+  },
+  {
+    "revision": "89c5ade69b6a72f43595ef69fe8d3838",
+    "url": "/ganalytics-dashboard/static/media/logo.89c5ade6.PNG"
+  },
+  {
+    "revision": "4b115e1153a9ea339d6a0bb284cc8ed3",
+    "url": "/ganalytics-dashboard/static/media/fa-brands-400.4b115e11.woff2"
+  },
+  {
+    "revision": "414ff5daad323a1c47c5177d4bd29674",
+    "url": "/ganalytics-dashboard/static/media/fa-regular-400.414ff5da.eot"
+  },
+  {
+    "revision": "5dd3976cb5d61e2e561f2a46b916f377",
+    "url": "/ganalytics-dashboard/static/media/fa-regular-400.5dd3976c.woff"
+  },
+  {
+    "revision": "d9d17590c975aad1be0ddab673f9c769",
+    "url": "/ganalytics-dashboard/static/media/fa-brands-400.d9d17590.eot"
+  },
+  {
+    "revision": "65779ebcc35604a25c2ba77309c5b8af",
+    "url": "/ganalytics-dashboard/static/media/fa-regular-400.65779ebc.woff2"
+  },
+  {
+    "revision": "adcefe34f10333ec2096",
+    "url": "/ganalytics-dashboard/static/js/main.adcefe34.chunk.js"
+  },
+  {
+    "revision": "f6c6f6c8cb7784254ad00056f6fbd74e",
+    "url": "/ganalytics-dashboard/static/media/fa-regular-400.f6c6f6c8.ttf"
+  },
+  {
+    "revision": "b90365bccdabd68c6c03902b4b141f09",
+    "url": "/ganalytics-dashboard/static/media/fa-brands-400.b90365bc.woff"
+  },
+  {
+    "revision": "462806316fea535a6a57651bc2b000b0",
+    "url": "/ganalytics-dashboard/static/media/fa-solid-900.46280631.woff2"
+  },
+  {
+    "revision": "61969d433bf265b9717a6c357a1e04e4",
+    "url": "/ganalytics-dashboard/static/media/fa-solid-900.61969d43.woff"
+  },
+  {
+    "revision": "b5596f4d339f99e3d69bc41be78db962",
+    "url": "/ganalytics-dashboard/static/media/fa-solid-900.b5596f4d.eot"
+  },
+  {
+    "revision": "e7e957c87c454bccaa3bf9fdaa6874f8",
+    "url": "/ganalytics-dashboard/static/media/fa-regular-400.e7e957c8.svg"
+  },
+  {
+    "revision": "80533988ff5fecd5be26557d08ce8237",
+    "url": "/ganalytics-dashboard/static/media/fa-brands-400.80533988.svg"
+  },
+  {
+    "revision": "b70cea0339374107969eb53e5b1f603f",
+    "url": "/ganalytics-dashboard/static/media/fa-solid-900.b70cea03.ttf"
+  },
+  {
+    "revision": "a7942393c52e76e3f85e",
+    "url": "/ganalytics-dashboard/static/css/2.609ecd61.chunk.css"
+  },
+  {
+    "revision": "2fbe672cfeebaf3e713622df9282b8ad",
+    "url": "/ganalytics-dashboard/index.html"
+  }
+];
