9582b0ff001e00fff96ad053fea0e9650a3d9afe
Xaconi <xaco.89@gmail.com>
2019-03-11 23:17:04 +0100

    [NEW] Footer content

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,28 @@ import React, { Component } from 'react'
 export class Footer extends Component {
     render() {
         return(
-            <footer></footer>
+            <footer>
+                <div className="columns">
+                    <div className="column is-offset-3 is-6">
+                        <p>
+                            Made with ❤️ by <a 
+                                href="https://twitter.com/xaconi" 
+                                ref="noreferrer noopener"
+                                target="_blank"
+                                title="Made with ❤️ by @Xaconi">
+                                @Xaconi
+                            </a>.
+                            Fork the code on <a 
+                                href="https://github.com/Xaconi/ganalytics-dashboard" 
+                                ref="noreferrer noopener"
+                                target="_blank"
+                                title="Fork the code on Github to improve this app!">
+                                Github
+                            </a> to improve this app!
+                        </p>
+                    </div>
+                </div>
+            </footer>
         )
     }
 }
